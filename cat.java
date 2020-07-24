5ab4196371eba5ec99d9c4533fb19fd4326a8ded
Akhil2626 <akhil.kanna199626@gmail.com>
2020-07-24 21:11:38 +0530

    added

--- cat.java
+++ cat.java
@@ -4,5 +4,8 @@ public static void main(String args[])
 {
 System.out.println("First12345vddsfdsfdsfcsd6789 java program");
 System.out.println("First12345vddsfdsfdsfcsd6789 java program");
+
+System.out.println("First12345vddsfdsfdsfcsd6789 java program");
+System.out.println("First12345vddsfdsfdsfcsd6789 java program");
 }
 }
--- king.java
+++ king.java
@@ -4,5 +4,15 @@ public static void main(String args[])
 {
 System.out.println("modified java program");
 System.out.println("modified java program");
+
+System.out.println("modified java program");
+System.out.println("modified java program");
+
+System.out.println("modified java program");
+System.out.println("modified java program");
+
+
+System.out.println("modified java program");
+System.out.println("modified java program");
 }
 }
